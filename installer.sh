daa0887e49d8f2c5f78452c37c8331778e3852a9
FelixFeuerigel <76600556+FelixFeuerigel@users.noreply.github.com>
2022-07-10 01:43:36 +0200

    small design change

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 # WARNING: this script will destroy data on the selected disk.
 # This script can be run by executing the following:
 ### curl -sL https://tinyurl.com/24tht5j5 | bash ###
-## if you need to use WiFi use "iwctl" for setup
+## if you need to use WiFi use "iwctl" for setup  ##
 
 set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
