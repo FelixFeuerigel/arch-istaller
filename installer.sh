9f5dd526106f03cd18b3d66da6d447ff0b2ed1df
FelixFeuerigel <76600556+FelixFeuerigel@users.noreply.github.com>
2022-07-10 01:21:36 +0200

    fix partitioning spacing

--- installer.sh
+++ installer.sh
@@ -55,7 +55,7 @@ if [ "$boot_mode" == "EFI" ]
     parted --script "${device}" -- mklabel gpt \
       mkpart ESP fat32 1Mib 300MiB \
       set 1 boot on \
-      mkpart primary linux-swap 129MiB ${swap_end} \
+      mkpart primary linux-swap 300MiB ${swap_end} \
       mkpart primary ext4 ${swap_end} 100%
 
     # Simple globbing was not enough as on one device I needed to match /dev/mmcblk0p1 
