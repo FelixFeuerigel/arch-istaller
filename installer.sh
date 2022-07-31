87cef96946b1193af51d2c37ef976e874ec23894
FelixFeuerigel <76600556+FelixFeuerigel@users.noreply.github.com>
2022-07-31 22:44:32 +0200

    change locale

--- installer.sh
+++ installer.sh
@@ -180,7 +180,7 @@ de_DE.UTF-8 UTF-8
 EOF
 
 arch-chroot /mnt locale-gen
-echo "LANG=en_US.UTF-8" >> /mnt/etc/locale.conf
+echo "LANG=de_DE.UTF-8" >> /mnt/etc/locale.conf
 echo "KEYMAP=de-latin1" >> /mnt/etc/vconsole.conf
 
 
